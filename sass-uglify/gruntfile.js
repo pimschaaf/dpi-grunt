b9cad86f2f9ee037ea4969dbb0f8c8290d1db462
Pim Schaaf <Pimschaaf@gmail.com>
2016-06-12 22:17:01 +0200

    Create gruntfile.js

---
+++ sass-uglify/gruntfile.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: { //task
+            options: { // Target options
+                outputStyle: 'compressed',
+                sourceMap: true
+            },
+            dist: { // Target
+                files: { // Dictionary of files
+                    '../assets/css/main.min.css': 'sass/main.scss' //target: source
+                }
+            }
+        },
+        uglify: {
+          dist: {
+            options: {
+              sourceMap: true
+            },
+            files: {
+              '../assets/js/main.min.js': 
+                [
+                  'js/functions.js',
+                  'js/ready.js'
+                ]
+            }
+          }
+        },
+        jshint: {
+            // define the files to lint
+            files: ['js/**/*.js'],
+            options: {
+//                ignores: [''],
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        },
+        watch: { //watch task
+            js: {
+                files: ['<%= jshint.files %>'],
+                tasks: ['jshint', 'uglify']
+            },
+            livereload: {
+	            // We use this target to watch files that will trigger the livereload
+                files: [
+                    // Anytime css is edited or compiled by sass, trigger the livereload on those files
+                    '../assets/css/*.css'
+                    // Or a js file
+                    '../assets/js/*.js'
+                ],
+                options: { livereload: true }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['jshint', 'uglify', 'sass']); //alias
+};
